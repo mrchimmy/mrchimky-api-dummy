c2775b23fedb53388246ff51268cb8c9210525cd
MrChim's <mrchimky@gmail.com>
2024-07-26 22:12:48 +0700

    build index

--- build/index.js
+++ build/index.js
@@ -3,6 +3,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const firebase_functions_1 = __importDefault(require("firebase-functions"));
-const server_1 = require("./server");
-exports.app = firebase_functions_1.default.https.onRequest(server_1.app);
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.app = (0, express_1.default)();
+const port = process.env.APP_PORT || 3001;
+exports.app.use((0, cors_1.default)());
+exports.app.use(body_parser_1.default.urlencoded({ extended: false }));
+exports.app.use(body_parser_1.default.json());
+const product_route_1 = __importDefault(require("./routes/product.route"));
+exports.app.get('/', (req, res) => {
+    return res.json({
+        message: "Hello World"
+    });
+});
+exports.app.use('/product', product_route_1.default);
+exports.app.listen(port, () => {
+    console.log(`Express is Running on Port : ${port}`);
+});
+module.exports = exports.app;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
-import functions from 'firebase-functions'
-import { app } from './server'
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from 'dotenv'
+dotenv.config()
+export const app = express();
+const port = process.env.APP_PORT || 3001;
 
-exports.app = functions.https.onRequest(app)
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+import productRouter from './routes/product.route';
+app.get('/', (req, res) => {
+    return res.json({
+      message: "Hello World"
+    })
+})
+app.use('/product', productRouter);
+
+app.listen(port, () => {
+  console.log(`Express is Running on Port : ${port}`);
+});
+
+module.exports = app;
